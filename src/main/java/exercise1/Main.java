82fd63a4be2da0f1a1a1c8d1fd0bafdaa3e23d5f
Michelle Zou <michelle_zou@hcl.com>
2021-06-30 13:06:36 -0400

    Added Exercise 1

---
+++ src/main/java/exercise1/Main.java
@@ -0,0 +1,26 @@
+package exercise1;
+
+import java.util.Scanner;
+
+/*
+ * Main class gets the input String and call the static method 
+ * checkCharacters present in the UserMainCode to check if the 
+ * first character of the input String is the same as the last 
+ * character.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Input String:");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int result = UserMainCode.checkCharacters(input);
+
+		if (result == 1) {
+			System.out.println("Valid");
+		} else {
+			System.out.println("Invalid");
+		}
+	}
+
+}
--- src/main/java/exercise1/UserMainCode.java
+++ src/main/java/exercise1/UserMainCode.java
@@ -1,5 +1,17 @@
 package exercise1;
 
+/*
+ * UserMainCode class includes a checkCharacters 
+ * method that checks if the first and last characters are same.
+ */
 public class UserMainCode {
 
+	static int checkCharacters(String str) {
+		char first = str.charAt(0);
+		char last = str.charAt(str.length()-1);
+		if (first == last) {
+			return 1;
+		}
+		return -1;
+	}
 }
