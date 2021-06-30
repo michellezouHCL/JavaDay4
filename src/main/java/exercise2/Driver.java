baefc0a1940c175060987b90106e40ee0bbe9196
Michelle Zou <michelle_zou@hcl.com>
2021-06-30 13:30:04 -0400

    Added Exercise 2

--- src/main/java/exercise2/Driver.java
+++ src/main/java/exercise2/Driver.java
@@ -4,7 +4,14 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String cat1 = "cat";
+		String cat2 = "cat";
 
+		System.out.println(cat1 == cat2);
+		
+		StringBuilder cat3 = new StringBuilder();
+		cat3.append("cat");
+		System.out.println(cat3.equals(cat1));
 	}
 
 }
