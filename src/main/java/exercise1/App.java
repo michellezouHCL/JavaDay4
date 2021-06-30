fa334d8b3d3a9fbdf78a9b0be9650d4a0a88224b
Michelle Zou <michelle_zou@hcl.com>
2021-06-30 12:34:37 -0400

    updated

---
+++ src/main/java/exercise1/App.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
